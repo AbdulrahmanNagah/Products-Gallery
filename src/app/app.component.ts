a565dfe213c709d3775f7c428ed20d4d1e526fa6
AbdulrahmanNagah <abdulrahman.nagah1@gmail.com>
2025-07-27 16:08:51 +0300

    errors and loading commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxSpinnerComponent } from "ngx-spinner";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, NgxSpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Ecommerce';
+ 
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 
-import { routes } from './app.routes';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { provideToastr } from 'ngx-toastr';
+import { routes } from './app.routes';
+import { errorsInterceptor } from './core/interceptors/errors.interceptor';
+import { loadingInterceptor } from './core/interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withViewTransitions(), withInMemoryScrolling({scrollPositionRestoration: "top"})),
+    provideRouter(routes, withViewTransitions(), 
+    withInMemoryScrolling({scrollPositionRestoration: "top"})
+  ),
      provideClientHydration(),
-     provideHttpClient(withFetch())
+     provideHttpClient(withFetch(), withInterceptors([loadingInterceptor, errorsInterceptor])),
+     provideToastr(),
+     provideAnimations(),
+     importProvidersFrom(NgxSpinnerModule),
     ]
 };
